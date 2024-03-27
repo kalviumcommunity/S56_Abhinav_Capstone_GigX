737853e17899e03f57df44c27ae3952d9591cb29
abhinav0306 <abhinavs0603@gmail.com>
2024-03-27 14:18:21 +0530

    changes made according to the review

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Styles/LoginPage.css";
 import google from "../assets/google.png";
 import loginimg from "../assets/loginimg.png";
 import { FaTimes } from 'react-icons/fa';
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+  const handleSignInSubmit = (event) => {
+    event.preventDefault();
+    navigate('/');
+  };
+
   return (
     <div>
       <div className="container">
@@ -13,36 +19,36 @@ const LoginPage = () => {
           <FaTimes />
         </Link>
         
-        <div className="flex-container flex">
-          <div className="box-left">
-            <h1>Welcome Back!</h1>
-            <p>Sign in to your account to continue</p>
-            <div className="input">
-              <input type="text" placeholder="Email" className='logininput' />
-              <input type="password" placeholder="Password" className='logininput' />
-            </div>
-            <button className='signinbtn'>Sign In</button>
-            <p className='forgot pointer'>Forgot Password?</p>
+        <form onSubmit={handleSignInSubmit}>
+          <div className="flex-container flex">
+            <div className="box-left">
+              <h1>Welcome Back!</h1>
+              <p>Sign in to your account to continue</p>
+              <div className="input">
+                <input type="email" placeholder="Email" className='logininput'required />
+                <input type="password" placeholder="Password" className='logininput' required />
+              </div>
+              <button type="submit" className='signinbtn'>Sign In</button>
+              <p className='forgot pointer'>Forgot Password?</p>
 
-            <p className='center'>Or</p>
-            <div className="google pointer">
-              <img src={google} alt="" />
-              <p>Continue With Google</p>
+              <p className='center'>Or</p>
+              <div className="google pointer">
+                <img src={google} alt="" />
+                <p>Continue With Google</p>
+              </div>
+            </div>
+            <div className="box-right">
+              <img src={loginimg} alt=" Login image" />
             </div>
           </div>
-          <div className="box-right">
+        </form>
 
-            <img src={loginimg} alt=" Login image" />
-            
-
-          </div>
-        </div>
         <p className='center'>
           Don't have an account? <Link to={"/signup"}> <span className='pointer signup'>Sign Up</span></Link>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginPage;
