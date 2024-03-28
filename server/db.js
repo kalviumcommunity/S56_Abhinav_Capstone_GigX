7646d162d35bd484db50a2450485f950af3316fe
abhinav0306 <abhinavs0603@gmail.com>
2024-03-28 16:49:53 +0530

    poolsize removed

--- server/db.js
+++ server/db.js
@@ -1,24 +1,14 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  
-  poolSize: 10,
-};
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.mongoURI, mongooseOptions);
-    console.log("Database Connected Successfully");
-  } catch (error) {
-    console.error("Database Connection Failed",error);
-  }
-};
-
-let mongooseConnect = () => {
-  return mongoose.connection.readyState === 1;
-};
-
-module.exports = { connectDB, mongooseConnect };
+const mongoose=require("mongoose")
+require("dotenv").config()
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.mongoURI)
+        console.log("Database Connected Successfully")
+    }catch(error){
+        console.error("Database Connection Failed",error)
+    }
+}
+let mongooseConnect=()=>{
+    return mongoose.connection.readyState===1;
+}
+module.exports={connectDB,mongooseConnect}
