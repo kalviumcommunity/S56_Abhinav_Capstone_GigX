1d6795d471356067641652111ee424d7ac635d47
abhinav0306 <abhinavs0603@gmail.com>
2024-04-03 16:20:34 +0530

    profile page added

--- server/Cloudinary.js
+++
@@ -1,7 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
-cloudinary.config({ 
-    cloud_name: 'dlfru944t', 
-    api_key: '314951961217529', 
-    api_secret: '' 
-  });
